dd47b6978c9dc77576d075268dd8853b8b85463e
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2024-08-20 21:42:01 +0100

    refactor: update models

--- toonkor_collector2/models.py
+++ toonkor_collector2/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+    
+
+class Manhwa(models.Model):
+    title = models.CharField(max_length=512)
+    author = models.CharField(max_length=512)
+    chapters = models.IntegerField(default=0)
+    banner = models.ImageField(blank=True)
+    url = models.URLField()
+
+
+class Chapter(models.Model):
+    manhwa = models.ForeignKey(Manhwa, on_delete=models.CASCADE)
+    translated = models.BooleanField(default=False)
+    path = models.FilePathField()
